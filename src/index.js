4ee6a08cdd3a7496a156ac8f188f272d312f828c
A Akhil <akhilappam01@gmail.com>
2026-02-11 09:31:06 +0530

    production config

--- src/index.js
+++ src/index.js
@@ -18,5 +18,5 @@ app.use("/meetings", meetingRoutes);
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log("Server running on " + PORT));
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 });
